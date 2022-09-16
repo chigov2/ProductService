62b59b995b7b483fc2e4cebe0833185a60c6e35c
chigov2 <86004573+chigov2@users.noreply.github.com>
2022-09-16 15:01:12 +0300

    1

--- src/main/java/com/appsdeveloperblog/estore/ProductService/rest/ProductsController.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductService/rest/ProductsController.java
@@ -1,9 +1,27 @@
 package com.appsdeveloperblog.estore.ProductService.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
+
+    @PostMapping
+    public String createProduct(){
+        return "Create product -> HTTP POST request handled";
+    }
+
+    @GetMapping
+    public String getProduct(){
+        return "Get product -> HTTP GET request handled";
+    }
+
+    @PutMapping
+    public String updateProduct(){
+        return "Update product -> HTTP PUT request handled";
+    }
+    @DeleteMapping
+    public String deleteProduct(){
+        return "Delete product -> HTTP Delete request handled";
+    }
 }
