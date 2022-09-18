d3d5a8cf50c968828b372d7a757f73830ee34230
chigov2 <86004573+chigov2@users.noreply.github.com>
2022-09-18 12:54:52 +0300

    works folder 7

--- src/main/java/com/appsdeveloperblog/estore/ProductService/rest/ProductsController.java
+++ src/main/java/com/appsdeveloperblog/estore/ProductService/rest/ProductsController.java
@@ -1,31 +1,57 @@
 package com.appsdeveloperblog.estore.ProductService.rest;
 
+import com.appsdeveloperblog.estore.ProductService.command.CreateProductCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
+
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
     @Autowired
-    private Environment env;
+    public ProductsController(Environment env, CommandGateway commandGateway) {
+        this.env = env;
+        this.commandGateway = commandGateway;
+    }
 
     @PostMapping
-    public String createProduct(){
-        return "Create product -> HTTP POST request handled";
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel){
+
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString()).build();
+
+        String returnValue = null;
+        try {
+            System.out.println("returnValue1 = " + returnValue);
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return returnValue;
     }
 
     @GetMapping
     public String getProduct(){
-        return "Get product -> HTTP GET request handled" + env.getProperty("local.server.port");
+        return "Get product -> HTTP GET request handled " + env.getProperty("local.server.port");
     }
 
     @PutMapping
     public String updateProduct(){
-        return "Update product -> HTTP PUT request handled";
+        return "Update product -> HTTP PUT request handled ";
     }
     @DeleteMapping
     public String deleteProduct(){
-        return "Delete product -> HTTP Delete request handled";
+        return "Delete product -> HTTP Delete request handled ";
     }
 }
